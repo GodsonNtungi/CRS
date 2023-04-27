beb6d13cd3e04759cadf1ad585c5defdd966f3ad
daud019 <daudmagembe341@gmail.com>
2023-04-27 06:58:01 -0700

    more

--- CropRecommendationSystem/app/views.py
+++ CropRecommendationSystem/app/views.py
@@ -7,26 +7,12 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html', {})
 
-
 def bylocation(request):
-<<<<<<< HEAD
     return render(request, 'bylocation.html', {})
-
-
-def bysoil(request):
-    return render(request, 'bysoil.html', {})
-
-
-def bycrop(request):
-    return render(request, 'bycrop.html', {})
 
 
 def services(request):
     return render(request, 'services.html')
-
-
-=======
-    return render(request, 'bylocation.html')
 
 
 def bysoil(request):
@@ -35,4 +21,4 @@ def bysoil(request):
 
 def bycrop(request):
     return render(request, 'bycrop.html')
->>>>>>> fc9c4496a838b5269544c36c35ffa0f954630b8d
+

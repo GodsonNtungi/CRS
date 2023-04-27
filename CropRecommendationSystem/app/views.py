3d19f500a2c79d1ed0e471b9308bf4907f2180f0
GodsonNtungi <godsonezekiel31@gmail.com>
2023-04-27 17:57:06 +0300

    new

--- CropRecommendationSystem/app/views.py
+++ CropRecommendationSystem/app/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
+import joblib as jb
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+import os
+import json
+
+from app.models import Crop
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # Create your views here.
 from django.shortcuts import render
 
+scaler: StandardScaler = jb.load(f"{BASE_DIR}/app/Models/scaler.joblib")
+model: RandomForestClassifier = jb.load(f"{BASE_DIR}/app/Models/modelv1.joblib")
+
 
 def home(request):
     return render(request, 'home.html', {})
+
 
 def bylocation(request):
     return render(request, 'bylocation.html', {})
@@ -20,5 +33,13 @@ def bysoil(request):
 
 
 def bycrop(request):
-    return render(request, 'bycrop.html')
-
+    if request.method == "GET":
+        return render(request, 'bycrop.html')
+    if request.method == "POST":
+        data = request.body
+        data_scaled = scaler.transform(data["N"], data["P"], data["K"], data["temperature"], data["humidity"],
+                                       data["ph"], data["rainfall"])
+        prediction = model.predict_proba(data_scaled)
+        output = [[model.classes_[index], round(value * 100, 2)] for index, value in enumerate(prediction[0])]
+        output.sort(key=lambda x: x[1], reverse=True)
+        return render(request, "bycrop.html", context={"data": output})
